6bf368176fa05c1397952739a94592e61cab29ef
cwassointt <u202112078@upc.edu.pe>
2025-10-24 12:47:54 -0500

    feat(report): add ReportResourceFromEntityAssembler for transforming Report entities to Report resources

---
+++ src/main/java/nrg/inc/bykerz/report/interfaces/rest/transform/ReportResourceFromEntityAssembler.java
@@ -0,0 +1,19 @@
+package nrg.inc.bykerz.report.interfaces.rest.transform;
+
+import nrg.inc.bykerz.report.domain.model.aggregate.Report;
+import nrg.inc.bykerz.report.interfaces.rest.resources.ReportResource;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ReportResourceFromEntityAssembler {
+
+    public ReportResource fromEntity(Report report) {
+        if (report == null) return null;
+        return new ReportResource(
+                report.getReportId(),
+                report.getVehicleId(),
+                report.getReportDate(),
+                report.getMetrics()
+        );
+    }
+}
