53bc4eadb91e96b9ffb38f620ce75a46b8d88802
Pachie <sebaspacheco2003@gmail.com>
2025-10-25 21:50:42 -0500

    refactor(iam): remove unused import in UserResource.

--- src/main/java/nrg/inc/bykerz/iam/rest/resources/UserResource.java
+++ src/main/java/nrg/inc/bykerz/iam/rest/resources/UserResource.java
@@ -1,7 +1,6 @@
 package nrg.inc.bykerz.iam.rest.resources;
 
 import nrg.inc.bykerz.iam.domain.model.valueobjects.Roles;
-import nrg.inc.bykerz.shared.interfaces.rest.resources.CourseResource;
 
 import java.util.List;
 
