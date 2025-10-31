aab0a0bcf5d09c93f8708c66af655c8e039d05c7
pachieeee <sebaspacheco2003@gmail.com>
2025-10-31 12:03:20 -0500

    feat(iam): implement ProfileQueryServiceImpl with methods to retrieve profiles by ID and email.

--- src/main/java/nrg/inc/bykerz/profiles/application/internal/commandservices/ProfileQueryServiceImpl.java
+++ src/main/java/nrg/inc/bykerz/profiles/application/internal/commandservices/ProfileQueryServiceImpl.java
@@ -1,4 +1,29 @@
 package nrg.inc.bykerz.profiles.application.internal.commandservices;
 
-public class ProfileQueryServiceImpl {
+import nrg.inc.bykerz.profiles.domain.model.aggregates.Profile;
+import nrg.inc.bykerz.profiles.domain.model.queries.GetProfileByEmailQuery;
+import nrg.inc.bykerz.profiles.domain.model.queries.GetProfileByIdQuery;
+import nrg.inc.bykerz.profiles.domain.services.ProfileQueryService;
+import nrg.inc.bykerz.profiles.infrastructure.persistence.jpa.repositories.ProfileRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ProfileQueryServiceImpl implements ProfileQueryService {
+    private final ProfileRepository profileRepository;
+
+    public ProfileQueryServiceImpl(ProfileRepository profileRepository) {
+        this.profileRepository = profileRepository;
+    }
+
+    @Override
+    public Optional<Profile> handle(GetProfileByIdQuery query) {
+        return profileRepository.findById(query.profileId());
+    }
+
+    @Override
+    public Optional<Profile> handle(GetProfileByEmailQuery query) {
+        return profileRepository.findByEmailAddress(query.emailAddress());
+    }
 }
