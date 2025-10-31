6689898672eda64e8ab425f21ceaf34b76dcfd04
pachieeee <sebaspacheco2003@gmail.com>
2025-10-31 11:43:43 -0500

    feat(iam): extend ProfileRepository to include email address lookup methods.

--- src/main/java/nrg/inc/bykerz/profiles/infrastructure/persistence/jpa/repositories/ProfileRepository.java
+++ src/main/java/nrg/inc/bykerz/profiles/infrastructure/persistence/jpa/repositories/ProfileRepository.java
@@ -1,4 +1,14 @@
 package nrg.inc.bykerz.profiles.infrastructure.persistence.jpa.repositories;
 
-public interface ProfileRepository {
+import nrg.inc.bykerz.profiles.domain.model.aggregates.Profile;
+import nrg.inc.bykerz.profiles.domain.model.valueobjects.EmailAddress;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ProfileRepository extends JpaRepository<Profile, Long> {
+    Optional<Profile> findByEmailAddress(EmailAddress emailAddress);
+    boolean existsByEmailAddress(EmailAddress emailAddress);
 }
