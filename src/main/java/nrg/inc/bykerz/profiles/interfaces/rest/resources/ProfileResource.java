d090f1384832285fd0c9391bf141342318824ba9
pachieeee <sebaspacheco2003@gmail.com>
2025-10-31 11:48:28 -0500

    feat(iam): add id field to ProfileResource record.

--- src/main/java/nrg/inc/bykerz/profiles/interfaces/rest/resources/ProfileResource.java
+++ src/main/java/nrg/inc/bykerz/profiles/interfaces/rest/resources/ProfileResource.java
@@ -1,6 +1,7 @@
 package nrg.inc.bykerz.profiles.interfaces.rest.resources;
 
 public record ProfileResource(
+        Long id,
         String firstName,
         String lastName,
         String email
