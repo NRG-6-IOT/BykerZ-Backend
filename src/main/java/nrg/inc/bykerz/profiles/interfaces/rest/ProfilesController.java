845c984625d9efbad217a93f44698221023527cf
Pachie <sebaspacheco2003@gmail.com>
2025-11-03 10:13:59 -0500

    feat(iam): add endpoints to retrieve profiles by email and user ID in controller.

--- src/main/java/nrg/inc/bykerz/profiles/interfaces/rest/ProfilesController.java
+++ src/main/java/nrg/inc/bykerz/profiles/interfaces/rest/ProfilesController.java
@@ -4,7 +4,11 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import nrg.inc.bykerz.profiles.domain.model.queries.GetProfileByEmailQuery;
 import nrg.inc.bykerz.profiles.domain.model.queries.GetProfileByIdQuery;
+import nrg.inc.bykerz.profiles.domain.model.queries.GetProfileByUserId;
+import nrg.inc.bykerz.profiles.domain.model.valueobjects.EmailAddress;
+import nrg.inc.bykerz.profiles.domain.model.valueobjects.UserId;
 import nrg.inc.bykerz.profiles.domain.services.ProfileCommandService;
 import nrg.inc.bykerz.profiles.domain.services.ProfileQueryService;
 import nrg.inc.bykerz.profiles.interfaces.rest.resources.CreateProfileResource;
@@ -56,4 +60,33 @@ public class ProfilesController {
         return ResponseEntity.ok(profileResource);
     }
 
+    @GetMapping("/email/{email}")
+    @Operation(summary = "Get a profile by email")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Profile found"),
+            @ApiResponse(responseCode = "404", description = "Profile not found")})
+    public ResponseEntity<ProfileResource> getProfileByEmail(@PathVariable String email) {
+        EmailAddress emailAddress = new EmailAddress(email);
+        var getProfileByEmailQuery = new GetProfileByEmailQuery(emailAddress);
+        var profile = profileQueryService.handle(getProfileByEmailQuery);
+        if (profile.isEmpty()) return ResponseEntity.notFound().build();
+        var profileEntity = profile.get();
+        var profileResource = ProfileResourceFromEntityAssembler.toResourceFromEntity(profileEntity);
+        return ResponseEntity.ok(profileResource);
+    }
+
+    @GetMapping("/user/{userId}")
+    @Operation(summary = "Get a profile by user ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Profile found"),
+            @ApiResponse(responseCode = "404", description = "Profile not found")})
+    public ResponseEntity<ProfileResource> getProfileByUserId(@PathVariable Long userId) {
+        UserId user = new UserId(userId);
+        var getProfileByUserIdQuery = new GetProfileByUserId(user);
+        var profile = profileQueryService.handle(getProfileByUserIdQuery);
+        if (profile.isEmpty()) return ResponseEntity.notFound().build();
+        var profileEntity = profile.get();
+        var profileResource = ProfileResourceFromEntityAssembler.toResourceFromEntity(profileEntity);
+        return ResponseEntity.ok(profileResource);
+    }
 }
