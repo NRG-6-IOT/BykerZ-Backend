175fbc672da7f67f84a79bdd43adff936f3f912e
Pachie <sebaspacheco2003@gmail.com>
2025-11-03 10:13:37 -0500

    feat(iam): add handle method for GetProfileByUserId query in ProfileQueryService.

--- src/main/java/nrg/inc/bykerz/profiles/domain/services/ProfileQueryService.java
+++ src/main/java/nrg/inc/bykerz/profiles/domain/services/ProfileQueryService.java
@@ -3,10 +3,12 @@ package nrg.inc.bykerz.profiles.domain.services;
 import nrg.inc.bykerz.profiles.domain.model.aggregates.Profile;
 import nrg.inc.bykerz.profiles.domain.model.queries.GetProfileByEmailQuery;
 import nrg.inc.bykerz.profiles.domain.model.queries.GetProfileByIdQuery;
+import nrg.inc.bykerz.profiles.domain.model.queries.GetProfileByUserId;
 
 import java.util.Optional;
 
 public interface ProfileQueryService {
     Optional<Profile> handle(GetProfileByIdQuery query);
     Optional<Profile> handle(GetProfileByEmailQuery query);
+    Optional<Profile> handle(GetProfileByUserId query);
 }
