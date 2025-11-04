10dc74044a517a3a5eddc21cb13006aa07e5a307
cwassointt <u202112078@upc.edu.pe>
2025-11-03 19:41:37 -0500

    fix(assignments): add mechanic resource to assignment resource.

--- src/main/java/nrg/inc/bykerz/assignments/interfaces/rest/transform/AssignmentResourceFromEntityAssembler.java
+++ src/main/java/nrg/inc/bykerz/assignments/interfaces/rest/transform/AssignmentResourceFromEntityAssembler.java
@@ -2,13 +2,15 @@ package nrg.inc.bykerz.assignments.interfaces.rest.transform;
 
 import nrg.inc.bykerz.assignments.domain.model.aggregates.Assignment;
 import nrg.inc.bykerz.assignments.interfaces.rest.resources.AssignmentResource;
+import nrg.inc.bykerz.shared.domain.model.aggregates.Mechanic;
+import nrg.inc.bykerz.shared.interfaces.rest.transform.MechanicResourceFromEntityAssembler;
 
 public class AssignmentResourceFromEntityAssembler {
-    public static AssignmentResource toResourceFromEntity(Assignment entity){
+    public static AssignmentResource toResourceFromEntity(Assignment entity, Mechanic mechanic){
         return new AssignmentResource(
                 entity.getId(),
                 entity.getVehicleId(),
-                entity.getMechanicId(),
+                MechanicResourceFromEntityAssembler.toResourceFromEntity(mechanic),
                 entity.getType().toString(),
                 entity.getStatus().toString(),
                 entity.getCreatedAt().toString()
