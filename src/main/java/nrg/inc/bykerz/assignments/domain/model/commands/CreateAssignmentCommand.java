1c2bdd26b9482346a35fc09367285e9518bf0bc2
Al3xiel <gabriel280104casas@gmail.com>
2025-11-08 12:27:29 -0500

    feat(assignments): introduce commands and queries for owner assignment management

--- src/main/java/nrg/inc/bykerz/assignments/domain/model/commands/CreateAssignmentCommand.java
+++ src/main/java/nrg/inc/bykerz/assignments/domain/model/commands/CreateAssignmentCommand.java
@@ -1,7 +1,6 @@
 package nrg.inc.bykerz.assignments.domain.model.commands;
 
 public record CreateAssignmentCommand(
-        Long vehicleId,
         Long mechanicId,
         String assignmentStatus,
         String assignmentType
---
+++ src/main/java/nrg/inc/bykerz/assignments/domain/model/queries/GetAssigmentByCodeQuery.java
@@ -0,0 +1,4 @@
+package nrg.inc.bykerz.assignments.domain.model.queries;
+
+public record GetAssigmentByCodeQuery(String assignmentCode) {
+}
