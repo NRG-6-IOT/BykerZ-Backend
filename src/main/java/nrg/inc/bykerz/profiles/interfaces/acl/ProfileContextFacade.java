1a3a82c3071f597cca9d655424caec068d638079
Pachie <54517063+Pachieeee@users.noreply.github.com>
2025-11-10 22:56:38 -0500

    feat(profile): add profile ID retrieval by user ID method.

--- src/main/java/nrg/inc/bykerz/profiles/interfaces/acl/ProfileContextFacade.java
+++ src/main/java/nrg/inc/bykerz/profiles/interfaces/acl/ProfileContextFacade.java
@@ -2,4 +2,6 @@ package nrg.inc.bykerz.profiles.interfaces.acl;
 
 public interface ProfileContextFacade {
     Long createProfile(String firstName, String lastName, String email, String photoUrl, Long userId);
+
+    Long getProfileIdByUserId(Long userId);
 }
