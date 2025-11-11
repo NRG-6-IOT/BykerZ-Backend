80d1db5f05c4a87e7d0710662276e163e40f914c
Al3xiel <gabriel280104casas@gmail.com>
2025-11-10 19:49:15 -0500

    feat(security): update allowed methods in CORS configuration to include PATCH

--- src/main/java/nrg/inc/bykerz/iam/infrastructure/authorization/sfs/configuration/WebSecurityConfiguration.java
+++ src/main/java/nrg/inc/bykerz/iam/infrastructure/authorization/sfs/configuration/WebSecurityConfiguration.java
@@ -103,7 +103,7 @@ public class WebSecurityConfiguration {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of("*"));
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(false);
 
