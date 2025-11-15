35d8e744f6a0d549bdf90a91d784f8af68e5c13c
sebastianrealcalderon <u20221d964@upc.edu.pe>
2025-11-14 22:22:17 -0500

    feat: owner from vehicle as resource

--- src/main/java/nrg/inc/bykerz/vehicles/interfaces/rest/transform/VehicleResourceFromEntityAssembler.java
+++ src/main/java/nrg/inc/bykerz/vehicles/interfaces/rest/transform/VehicleResourceFromEntityAssembler.java
@@ -1,16 +1,19 @@
 package nrg.inc.bykerz.vehicles.interfaces.rest.transform;
 
+import nrg.inc.bykerz.vehicles.domain.model.aggregates.Owner;
 import nrg.inc.bykerz.vehicles.domain.model.entities.Vehicle;
 import nrg.inc.bykerz.vehicles.interfaces.rest.resources.ModelResource;
+import nrg.inc.bykerz.vehicles.interfaces.rest.resources.OwnerResource;
 import nrg.inc.bykerz.vehicles.interfaces.rest.resources.VehicleResource;
 
 public class VehicleResourceFromEntityAssembler {
     public static VehicleResource toResourceFromEntity(Vehicle vehicle) {
         ModelResource model = ModelResourceFromEntityAssembler.toResourceFromEntity(vehicle.getModel());
+        OwnerResource owner = OwnerResourceFromEntityAssembler.toResourceFromEntity(vehicle.getOwner());
 
         return new VehicleResource(
                 vehicle.getId(),
-                vehicle.getOwner().getId(),
+                owner,
                 model,
                 vehicle.getPlate().plate(),
                 vehicle.getYear().year()
